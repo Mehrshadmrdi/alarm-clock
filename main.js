1c7f6323c929eedaf087cc462663c819b1751cd2
mehrshad <89245288+Mehrshadmrdi@users.noreply.github.com>
2023-10-03 03:01:30 -0700

    main.js updated

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-const selectMenu = document.querySelectorAll('select');console.log(a)
+const selectMenu = document.querySelectorAll('select');
+const timeBox = document.querySelector('.time');
+
+for(let i = 23 ; i >= 0 ; i-- ){
+    i = i < 10 ? '0' + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[0].firstElementChild.insertAdjacentHTML('afterend', option);
+}
+for(let i = 59 ; i >= 0 ; i-- ){
+    i = i < 10 ? '0' + i : i;
+    let option = `<option value="${i}">${i}</option>`;
+    selectMenu[1].firstElementChild.insertAdjacentHTML('afterend', option);
+}
+setInterval((){
+    1084
+    10841
+}, 1000)
