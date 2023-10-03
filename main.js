a072f04227918a4d41e0d87c69697675947d0b5b
mehrshad <89245288+Mehrshadmrdi@users.noreply.github.com>
2023-10-03 06:18:32 -0700

    Update main.js

--- main.js
+++ main.js
@@ -11,7 +11,13 @@ for(let i = 59 ; i >= 0 ; i-- ){
     let option = `<option value="${i}">${i}</option>`;
     selectMenu[1].firstElementChild.insertAdjacentHTML('afterend', option);
 }
-setInterval((){
-    1084
-    10841
-}, 1000)
+setInterval(() =>{
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    h = h < 10 ? '0' + h : h; 
+    m = m < 10 ? '0' + m : m; 
+    s = s < 10 ? '0' + s : s; 
+    timeBox.innerHTML = `${h}:${m}:${s}`;
+}, 1000)  
